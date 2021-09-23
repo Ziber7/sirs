76f5b90a7765c982b95ef09851c3f710fb8343e1
Ziber7 <naufaljundi.2019@student.uny.ac.id>
2021-09-23 08:49:03 +0700

    Database Configuration

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('home') }}" class="brand-link">
-        <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
+        <img src="https://w7.pngwing.com/pngs/957/974/png-transparent-hospital-logo-clinic-health-care-physician-business-thumbnail.png"
              alt="{{ config('app.name') }} Logo"
              class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -412,7 +412,7 @@
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
             
-            <h1 style="color: white;">
+            <h1 style="color: black;">
                 Sistem Informasi Rumah Sakit
             </h1>
         </div>
